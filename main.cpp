5f5efb5bf8d4f04572e6ed6525aa5b8cfa67ab4e
Vincent <vijo1903@student.miun.se>
2022-12-11 00:39:01 +0100

    Further implementation

--- main.cpp
+++ main.cpp
@@ -12,25 +12,32 @@ struct ageGreaterThan30 {
     }
 };
 
+
 int main() {
     std::vector<Person> persons;
-
+    MyPrint print;
 
     persons.push_back({"Vincent Johansson", 22});
     persons.push_back({"Gillian Persson", 21});
     persons.push_back({"Andreas Perkuleel", 27});
-    persons.push_back({"Anton oskarsson", 29});
-    persons.push_back({"Pita Rolifsson", 30});
+    persons.push_back({"Anton oskarsson", 30});
+    persons.push_back({"Pita Rolifsson", 31});
 
+    std::cout << "For_each" << "\n";
     std::for_each(persons.begin(), persons.end(), [](Person person) {
         MyPrint print;
         print(person);
         std::cout << "\n";
     });
 
+    std::cout << "Age greater than 30" << "\n";
     auto found = std::find_if(persons.begin(), persons.end(), ageGreaterThan30());
 
-    std::cout << found->getName();
+    if (found != persons.end())
+        print(*found);
+
+
+
 
     return 0;
 }
